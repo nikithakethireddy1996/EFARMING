ba7c0b49951028d13874954a42a7653b43331b32
FelipeGHB <s531355@nwmissouri.edu>
2020-03-02 19:17:46 -0600

    Added Crop Analysis Portions: Crop Analysis main page Crop Analysis recycle view page Crop Analysis input form

--- app/src/main/java/com/example/efarming/ui/crop_analysis/CropAnalysisFragment.java
+++ app/src/main/java/com/example/efarming/ui/crop_analysis/CropAnalysisFragment.java
@@ -1,9 +1,11 @@
 package com.example.efarming.ui.crop_analysis;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -12,10 +14,12 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.efarming.CA_RecucleView_Page_Activity;
 import com.example.efarming.R;
+import com.example.efarming.ui.crop_analysis.CropAnalysisViewModel;
 
 public class CropAnalysisFragment extends Fragment {
-
+    Button blackBTN;
     private CropAnalysisViewModel cropAnalysisViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -23,11 +27,31 @@ public class CropAnalysisFragment extends Fragment {
         cropAnalysisViewModel =
                 ViewModelProviders.of(this).get(CropAnalysisViewModel.class);
         View root = inflater.inflate(R.layout.fragment_crop_analysis, container, false);
-        final TextView textView = root.findViewById(R.id.text_gallery);
-        cropAnalysisViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        blackBTN = (Button) root.findViewById(R.id.BlackSoilBTN);
+        Button redBTN = root.findViewById(R.id.RedSoilBTN);
+        Button sandyBTN = root.findViewById(R.id.SandySoilBTN);
+
+        blackBTN.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View v) {
+                Intent ini1 = new Intent(getActivity(), CA_RecucleView_Page_Activity.class);
+                startActivity(ini1);
+            }
+        });
+
+        redBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent ini1 = new Intent(getActivity(), CA_RecucleView_Page_Activity.class);
+                startActivity(ini1);
+            }
+        });
+
+        sandyBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent ini1 = new Intent(getActivity(), CA_RecucleView_Page_Activity.class);
+                startActivity(ini1);
             }
         });
         return root;
