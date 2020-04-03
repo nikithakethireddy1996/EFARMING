90fa9c402a0795bf46592505f24d0c1c6d15178b
nikithakethireddy1996 <54380887+nikithakethireddy1996@users.noreply.github.com>
2020-04-03 18:46:05 -0500

    added restrictions that all fields must be filled in user crop activity

--- app/src/main/java/com/example/efarming/UserCropInfoActivity.java
+++ app/src/main/java/com/example/efarming/UserCropInfoActivity.java
@@ -14,19 +14,33 @@ import android.widget.Toast;
 public class UserCropInfoActivity extends AppCompatActivity {
     private Button button1;
     EditText setCropNameET;
+    EditText setYearCultivatedET;
+    EditText setQuantityCultivatedET;
+    EditText setProfitObtainedET;
+    EditText setAmountSpendET;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_crop_info);
         setCropNameET = (EditText) findViewById(R.id.setCropNameET);
+        setYearCultivatedET = (EditText) findViewById(R.id.setYearCultivatedET);
+        setQuantityCultivatedET=(EditText) findViewById(R.id.setQuantityCultivatedET);
+        setProfitObtainedET=(EditText)findViewById(R.id.setProfitObtainedET);
+        setAmountSpendET=(EditText)findViewById(R.id.setAmountSpendET);
         button1 = (Button) findViewById(R.id.saveBTN);
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (setCropNameET.length()!=0) {
+               /* if (setCropNameET.length()!=0) {
                     Toast toast = Toast.makeText(getApplicationContext(), "Saved successfully", Toast.LENGTH_SHORT);
                     toast.show();
+                }*/
+
+                if (setCropNameET.length()==0 ||  setQuantityCultivatedET.length()==0
+                        || setProfitObtainedET.length()==0 || setAmountSpendET.length()==0){
+                    Toast.makeText(getApplicationContext(),"All the required fields must be filled",Toast.LENGTH_SHORT).show();
+                    return;
                 }
                 Intent in = new Intent(UserCropInfoActivity.this, MainActivity.class);
                 startActivity(in);
