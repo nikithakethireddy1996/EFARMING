f31e8e6ce281217bf852f704640793b5ab3591ef
yugimamidi <yugi.mamidi01@gmail.com>
2020-04-02 23:44:11 -0500

    updated with validation in forgot password activity

--- app/src/main/java/com/example/efarming/ForgotPasswordActivity.java
+++ app/src/main/java/com/example/efarming/ForgotPasswordActivity.java
@@ -7,20 +7,30 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class ForgotPasswordActivity extends AppCompatActivity {
     private Button button2;
+    EditText EmailTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
         button2 = (Button) findViewById(R.id.backBTN);
+        EmailTxt = (EditText) findViewById(R.id.EmailTxt);
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (!isEmailValid(EmailTxt.getText().toString())){
+                    Toast.makeText(getApplicationContext(),"Email address should be of valid format",Toast.LENGTH_LONG).show();
+                    return;
+                }
                 moveToLoginActivity();
             }
         });
@@ -29,6 +39,13 @@ public class ForgotPasswordActivity extends AppCompatActivity {
     private void moveToLoginActivity() {
         Intent in = new Intent(ForgotPasswordActivity.this, LoginActivity.class);
         startActivity(in);
+    }
+
+    public static boolean isEmailValid(final String email) {
+        final String expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
+        Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
     }
 
     protected void onStop() {
