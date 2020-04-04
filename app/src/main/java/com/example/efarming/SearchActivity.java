35c6b92d4e668fd9e47b0bb9a7e793657249ceec
yugimamidi <yugi.mamidi01@gmail.com>
2020-04-04 11:24:10 -0500

    updated by creating fragment for map

--- app/src/main/java/com/example/efarming/SearchActivity.java
+++ app/src/main/java/com/example/efarming/SearchActivity.java
@@ -1,14 +1,23 @@
 package com.example.efarming;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
 
-public class SearchActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+public class SearchActivity extends FragmentActivity implements OnMapReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        
     }
 }
