6e32b87b39abb520f08c2d9b15282fff9ef78b96
yugimamidi <yugi.mamidi01@gmail.com>
2020-04-21 09:34:34 -0500

    updated with maps data in search activity

--- app/src/main/java/com/example/efarming/SearchActivity.java
+++ app/src/main/java/com/example/efarming/SearchActivity.java
@@ -52,26 +52,216 @@ public class SearchActivity extends FragmentActivity implements OnMapReadyCallba
             @Override
             public boolean onQueryTextSubmit(String query) {
                 String location = searchView.getQuery().toString();
-
-                if(location != null || !location.equals("") || location.equals("grapes")){
-                    latlngs.add(new LatLng(38.384401, -100.782266));
-                    latlngs.add(new LatLng(39.683452, -97.666177));
-                    latlngs.add(new LatLng(40.333804, -94.872031));
-                    latlngs.add(new LatLng(39.108342, -94.614589));
-                    latlngs.add(new LatLng(38.181654, -97.339199));
-                    LatLngBounds.Builder builder = new LatLngBounds.Builder();
-                    for (LatLng p: latlngs){
-                        options.position(p);
-                        options.title("grapes");
-                        options.snippet("grapesInfo");
-                        map.addMarker(options);
-                        builder.include(options.getPosition());
+                    if (location != null && !location.equals("") && location.equalsIgnoreCase("grapes")) {
+                        latlngs.clear();
+                        map.clear();
+                        latlngs.add(new LatLng(38.384401, -100.782266));
+                        latlngs.add(new LatLng(39.683452, -97.666177));
+                        latlngs.add(new LatLng(40.333804, -94.872031));
+                        latlngs.add(new LatLng(39.108342, -94.614589));
+                        latlngs.add(new LatLng(38.181654, -97.339199));
+                        LatLngBounds.Builder builder = new LatLngBounds.Builder();
+                        for (LatLng p : latlngs) {
+                            options.position(p);
+                            options.title("grapes");
+                            options.snippet("grapesInfo");
+                            map.addMarker(options);
+                            builder.include(options.getPosition());
+                        }
+                        LatLngBounds bounds = builder.build();
+                        // offset from edges of the map in pixels
+                        CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds, 100);
+                        map.moveCamera(cu);
                     }
-                    LatLngBounds bounds = builder.build();
-                   // offset from edges of the map in pixels
-                    CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds, 100);
-                    map.moveCamera(cu);
-                }
+                    if (location != null && !location.equals("") && location.equalsIgnoreCase("mangoes")) {
+                        latlngs.clear();
+                        map.clear();
+                        latlngs.add(new LatLng(38.384401, -100.782266));
+                        latlngs.add(new LatLng(39.683452, -97.666177));
+                        latlngs.add(new LatLng(40.333804, -94.872031));
+                        latlngs.add(new LatLng(39.108342, -94.614589));
+                        latlngs.add(new LatLng(38.181654, -97.339199));
+                        LatLngBounds.Builder builder = new LatLngBounds.Builder();
+                        for (LatLng p : latlngs) {
+                            options.position(p);
+                            options.title("mangoes");
+                            options.snippet("mangoesInfo");
+                            map.addMarker(options);
+                            builder.include(options.getPosition());
+                        }
+                        LatLngBounds bounds = builder.build();
+                        // offset from edges of the map in pixels
+                        CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds, 100);
+                        map.moveCamera(cu);
+                    }
+                    if (location != null && !location.equals("") && location.equalsIgnoreCase("oranges")) {
+                        latlngs.clear();
+                        map.clear();
+                        latlngs.add(new LatLng(38.384401, -100.782266));
+                        latlngs.add(new LatLng(39.683452, -97.666177));
+                        latlngs.add(new LatLng(40.333804, -94.872031));
+                        latlngs.add(new LatLng(39.108342, -94.614589));
+                        latlngs.add(new LatLng(38.181654, -97.339199));
+                        LatLngBounds.Builder builder = new LatLngBounds.Builder();
+                        for (LatLng p : latlngs) {
+                            options.position(p);
+                            options.title("oranges");
+                            options.snippet("orangesInfo");
+                            map.addMarker(options);
+                            builder.include(options.getPosition());
+                        }
+                        LatLngBounds bounds = builder.build();
+                        // offset from edges of the map in pixels
+                        CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds, 100);
+                        map.moveCamera(cu);
+                    }
+                    if (location != null && !location.equals("") && location.equalsIgnoreCase("bananas")) {
+                        latlngs.clear();
+                        map.clear();
+                        latlngs.add(new LatLng(38.384401, -100.782266));
+                        latlngs.add(new LatLng(39.683452, -97.666177));
+                        latlngs.add(new LatLng(40.333804, -94.872031));
+                        latlngs.add(new LatLng(39.108342, -94.614589));
+                        latlngs.add(new LatLng(38.181654, -97.339199));
+                        LatLngBounds.Builder builder = new LatLngBounds.Builder();
+                        for (LatLng p : latlngs) {
+                            options.position(p);
+                            options.title("bananas");
+                            options.snippet("bananasInfo");
+                            map.addMarker(options);
+                            builder.include(options.getPosition());
+                        }
+                        LatLngBounds bounds = builder.build();
+                        // offset from edges of the map in pixels
+                        CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds, 100);
+                        map.moveCamera(cu);
+                    }
+                    if (location != null && !location.equals("") && location.equalsIgnoreCase("apples")) {
+                        latlngs.clear();
+                        map.clear();
+                        latlngs.add(new LatLng(38.384400, -100.782267));
+                        latlngs.add(new LatLng(39.683453, -97.666178));
+                        latlngs.add(new LatLng(40.333805, -94.872032));
+                        latlngs.add(new LatLng(39.108343, -94.614580));
+                        latlngs.add(new LatLng(38.181655, -97.339190));
+                        LatLngBounds.Builder builder = new LatLngBounds.Builder();
+                        for (LatLng p : latlngs) {
+                            options.position(p);
+                            options.title("apples");
+                            options.snippet("applesInfo");
+                            map.addMarker(options);
+                            builder.include(options.getPosition());
+                        }
+                        LatLngBounds bounds = builder.build();
+                        // offset from edges of the map in pixels
+                        CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds, 100);
+                        map.moveCamera(cu);
+                    }
+                    if (location != null && !location.equals("") && location.equalsIgnoreCase("carrot")) {
+                        latlngs.clear();
+                        map.clear();
+                        latlngs.add(new LatLng(38.384401, -100.782266));
+                        latlngs.add(new LatLng(39.683452, -97.666177));
+                        latlngs.add(new LatLng(40.333804, -94.872031));
+                        latlngs.add(new LatLng(39.108342, -94.614589));
+                        latlngs.add(new LatLng(38.181654, -97.339199));
+                        LatLngBounds.Builder builder = new LatLngBounds.Builder();
+                        for (LatLng p : latlngs) {
+                            options.position(p);
+                            options.title("carrot");
+                            options.snippet("carrotInfo");
+                            map.addMarker(options);
+                            builder.include(options.getPosition());
+                        }
+                        LatLngBounds bounds = builder.build();
+                        // offset from edges of the map in pixels
+                        CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds, 100);
+                        map.moveCamera(cu);
+                    }
+                    if (location != null && !location.equals("") && location.equalsIgnoreCase("cucumber")) {
+                        latlngs.clear();
+                        map.clear();
+                        latlngs.add(new LatLng(38.384401, -100.782266));
+                        latlngs.add(new LatLng(39.683452, -97.666177));
+                        latlngs.add(new LatLng(40.333804, -94.872031));
+                        latlngs.add(new LatLng(39.108342, -94.614589));
+                        latlngs.add(new LatLng(38.181654, -97.339199));
+                        LatLngBounds.Builder builder = new LatLngBounds.Builder();
+                        for (LatLng p : latlngs) {
+                            options.position(p);
+                            options.title("cucumber");
+                            options.snippet("cucumberInfo");
+                            map.addMarker(options);
+                            builder.include(options.getPosition());
+                        }
+                        LatLngBounds bounds = builder.build();
+                        // offset from edges of the map in pixels
+                        CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds, 100);
+                        map.moveCamera(cu);
+                    }
+                    if (location != null && !location.equals("") && location.equalsIgnoreCase("spinach")) {
+                        latlngs.clear();
+                        map.clear();
+                        latlngs.add(new LatLng(38.384401, -100.782266));
+                        latlngs.add(new LatLng(39.683452, -97.666177));
+                        latlngs.add(new LatLng(40.333804, -94.872031));
+                        latlngs.add(new LatLng(39.108342, -94.614589));
+                        latlngs.add(new LatLng(38.181654, -97.339199));
+                        LatLngBounds.Builder builder = new LatLngBounds.Builder();
+                        for (LatLng p : latlngs) {
+                            options.position(p);
+                            options.title("spinach");
+                            options.snippet("spinachInfo");
+                            map.addMarker(options);
+                            builder.include(options.getPosition());
+                        }
+                        LatLngBounds bounds = builder.build();
+                        // offset from edges of the map in pixels
+                        CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds, 100);
+                        map.moveCamera(cu);
+                    }
+                    if (location != null && !location.equals("") && location.equalsIgnoreCase("capsicum")) {
+                        latlngs.clear();
+                        map.clear();
+                        latlngs.add(new LatLng(38.384408, -100.782269));
+                        latlngs.add(new LatLng(39.683454, -97.666179));
+                        latlngs.add(new LatLng(40.333808, -94.872036));
+                        latlngs.add(new LatLng(39.108344, -94.614585));
+                        latlngs.add(new LatLng(38.181656, -97.339195));
+                        LatLngBounds.Builder builder = new LatLngBounds.Builder();
+                        for (LatLng p : latlngs) {
+                            options.position(p);
+                            options.title("capsicum");
+                            options.snippet("capsicumInfo");
+                            map.addMarker(options);
+                            builder.include(options.getPosition());
+                        }
+                        LatLngBounds bounds = builder.build();
+                        // offset from edges of the map in pixels
+                        CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds, 100);
+                        map.moveCamera(cu);
+                    }
+                    if (location != null && !location.equals("") && location.equalsIgnoreCase("potato")) {
+                        latlngs.clear();
+                        map.clear();
+                        latlngs.add(new LatLng(38.384408, -100.782269));
+                        latlngs.add(new LatLng(39.683454, -97.666179));
+                        latlngs.add(new LatLng(40.333808, -94.872036));
+                        latlngs.add(new LatLng(39.108344, -94.614585));
+                        latlngs.add(new LatLng(38.181656, -97.339195));
+                        LatLngBounds.Builder builder = new LatLngBounds.Builder();
+                        for (LatLng p : latlngs) {
+                            options.position(p);
+                            options.title("potato");
+                            options.snippet("potatoInfo");
+                            map.addMarker(options);
+                            builder.include(options.getPosition());
+                        }
+                        LatLngBounds bounds = builder.build();
+                        // offset from edges of the map in pixels
+                        CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds, 100);
+                        map.moveCamera(cu);
+                    }
                 return false;
             }
 
@@ -87,7 +277,7 @@ public class SearchActivity extends FragmentActivity implements OnMapReadyCallba
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        map=googleMap;
+        map = googleMap;
     }
-    
+
 }
