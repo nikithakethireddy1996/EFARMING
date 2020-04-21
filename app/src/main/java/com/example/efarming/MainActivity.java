9964351b85ba3b9a2bde786d5df6a20fdf2dc7d4
yugimamidi <yugi.mamidi01@gmail.com>
2020-04-21 18:42:26 -0500

    updated main activity to check a condition

--- app/src/main/java/com/example/efarming/MainActivity.java
+++ app/src/main/java/com/example/efarming/MainActivity.java
@@ -119,8 +119,8 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
         if (id == R.id.action_settings) {
-            FirebaseAuth.getInstance().signOut();
             LoginActivity.isUserLogin = false;
+            FirebaseAuth.getInstance().signOut();
             TextView username, email;
             username = (TextView) navHeader.findViewById(R.id.textName);
             email = (TextView) navHeader.findViewById(R.id.textEmail);
