b86b54092c1887d915121cf3edc682725efbb897
yugimamidi <yugi.mamidi01@gmail.com>
2020-04-21 11:59:29 -0500

    updated to show name and email on navigation main header

--- app/src/main/java/com/example/efarming/LoginActivity.java
+++ app/src/main/java/com/example/efarming/LoginActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.LinkMovementMethod;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,6 +18,11 @@ import com.google.android.gms.tasks.Task;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
 
 
 public class LoginActivity extends AppCompatActivity {
@@ -25,10 +31,8 @@ public class LoginActivity extends AppCompatActivity {
     private EditText passwordET;
     public static String userName, emailId = "";
     public static boolean isUserLogin = false;
-
-
+    FirebaseFirestore firebaseFirestore;
     FirebaseAuth fAuth;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +42,7 @@ public class LoginActivity extends AppCompatActivity {
         passwordET = (EditText) findViewById(R.id.passwordET);
         loginBTN = (Button) findViewById(R.id.loginBTN);
         fAuth = FirebaseAuth.getInstance();
+        firebaseFirestore = FirebaseFirestore.getInstance();
         TextView register = (TextView) findViewById(R.id.lnkRegisterTV);
         register.setMovementMethod(LinkMovementMethod.getInstance());
         register.setOnClickListener(new View.OnClickListener() {
@@ -60,13 +65,25 @@ public class LoginActivity extends AppCompatActivity {
                     Toast.makeText(getApplicationContext(), "password must be filled", Toast.LENGTH_SHORT).show();
                     return;
                 }
+                firebaseFirestore.collection("usersList").addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(QuerySnapshot documentSnapshots, FirebaseFirestoreException e) {
+
+                        for (DocumentChange documentChange : documentSnapshots.getDocumentChanges())
+                        {
+                            if (email.equalsIgnoreCase(documentChange.getDocument().getData().get("EmailId").toString())){
+                                userName =  documentChange.getDocument().getData().get("FirstName").toString();
+                                userName = userName.substring(0,1).toUpperCase()+userName.substring(1);
+                                break;
+                            }
+                        }
+                    }
+                });
                 fAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             isUserLogin = true;
-                            String[] user = email.split("@");
-                            userName = user[0];
                             emailId = email;
                             Toast.makeText(getApplicationContext(), "Logged in Successfully!", Toast.LENGTH_SHORT).show();
                             Intent intent = new Intent(LoginActivity.this, MainActivity.class);
